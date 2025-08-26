127fe9e795f5a5794320543d0e9e5c2cac5e8a0e
Marko <marcosvelasquezvela123@gmail.com>
2025-08-26 00:36:52 -0400

    cajero

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 import { Login } from './modules/login/login';
 import { Home } from './modules/home/home';
+import { Cajero } from './modules/cajero/cajero';
 
 export const routes: Routes = [
-    { path: '', component: Home },
     {
-        path: 'login',
+        path: '',
         component: Login,
     },
+    {
+      path: 'cajero', component: Cajero 
+    }
 ];
