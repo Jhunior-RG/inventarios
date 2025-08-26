2188563c68c6abb9aff2df0298814881d0657c5b
Jhunior Garcia <jhuniorremberto1@gmail.com>
2025-08-25 23:02:00 -0400

    lucide icons

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LucideAngularModule, PenIcon } from "lucide-angular";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LucideAngularModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
+  readonly PenIcon = PenIcon;
   protected readonly title = signal('inventarios');
 }
